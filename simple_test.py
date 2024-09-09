fd376894d82993295ee1980169b62349ef1322c3
alafage <adrienlafage@outlook.com>
2024-09-09 16:56:35 +0200

    :construction:

--- simple_test.py
+++ simple_test.py
@@ -4,7 +4,7 @@ from utils.model_utils import load_llama_tokenizer, load_model
 
 model_name = "baffo32/decapoda-research-llama-7B-hf"
 
-#print("Loading model...")
+print("Loading model...")
 
 # model = load_model(
 #         base_model=model_name,
@@ -14,9 +14,9 @@ model_name = "baffo32/decapoda-research-llama-7B-hf"
 #         lora_target_modules=("q_proj", "k_proj", "v_proj", "o_proj"),
 #         resume_from_checkpoint="models/weights/stage2_with_pretrained/",
 #     )
-#model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=BitsAndBytesConfig(load_in_8bit=True), local_files_only=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=BitsAndBytesConfig(load_in_8bit=True), local_files_only=True)
 
-#print("Model loaded.")
+print("Model loaded.")
 
 # model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
 
