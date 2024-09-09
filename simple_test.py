bb49777594e7e4cbad312f951902d4a6285404af
alafage <adrienlafage@outlook.com>
2024-09-09 15:15:43 +0200

    Add model loading script and print loading messages

--- simple_test.py
+++ simple_test.py
@@ -3,6 +3,8 @@ from utils.model_utils import load_llama_tokenizer, load_model
 
 model_name = "baffo32/decapoda-research-llama-7B-hf"
 
+print("Loading model...")
+
 model = load_model(
         base_model=model_name,
         lora_r=16,
@@ -12,7 +14,13 @@ model = load_model(
         resume_from_checkpoint="models/weights/stage2_with_pretrained/",
     )
 
+print("Model loaded.")
+
 model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
+
+print("Loading tokenizer...")
 
 # Load tokenizer
 tokenizer = load_llama_tokenizer(model_name)
+
+print("Tokenizer loaded.")
