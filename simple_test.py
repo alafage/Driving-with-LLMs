b7575661705e7a06749ca6987636a9d3238b8b8b
alafage <adrienlafage@outlook.com>
2024-09-09 15:48:25 +0200

    Refactor model loading and tokenizer in simple_test.py

--- simple_test.py
+++ simple_test.py
@@ -1,3 +1,4 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from utils.model_utils import load_llama_tokenizer, load_model
 
 
@@ -5,14 +6,15 @@ model_name = "baffo32/decapoda-research-llama-7B-hf"
 
 print("Loading model...")
 
-model = load_model(
-        base_model=model_name,
-        lora_r=16,
-        lora_alpha=16,
-        lora_dropout=0.05,
-        lora_target_modules=("q_proj", "k_proj", "v_proj", "o_proj"),
-        resume_from_checkpoint="models/weights/stage2_with_pretrained/",
-    )
+# model = load_model(
+#         base_model=model_name,
+#         lora_r=16,
+#         lora_alpha=16,
+#         lora_dropout=0.05,
+#         lora_target_modules=("q_proj", "k_proj", "v_proj", "o_proj"),
+#         resume_from_checkpoint="models/weights/stage2_with_pretrained/",
+#     )
+model = AutoModelForCausalLM.from_pretrained(model_name)
 
 print("Model loaded.")
 
@@ -21,6 +23,7 @@ model.print_trainable_parameters()  # Be more transparent about the % of trainab
 print("Loading tokenizer...")
 
 # Load tokenizer
-tokenizer = load_llama_tokenizer(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+# tokenizer = load_llama_tokenizer(model_name)
 
 print("Tokenizer loaded.")
