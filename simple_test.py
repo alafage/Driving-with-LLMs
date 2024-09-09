90a34b95dd5436f96f152ee9d4e2ebb86ea17113
alafage <adrienlafage@outlook.com>
2024-09-09 16:45:57 +0200

    :construction:

--- simple_test.py
+++ simple_test.py
@@ -1,10 +1,10 @@
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from utils.model_utils import load_llama_tokenizer, load_model
 
 
 model_name = "baffo32/decapoda-research-llama-7B-hf"
 
-print("Loading model...")
+#print("Loading model...")
 
 # model = load_model(
 #         base_model=model_name,
@@ -14,15 +14,16 @@ print("Loading model...")
 #         lora_target_modules=("q_proj", "k_proj", "v_proj", "o_proj"),
 #         resume_from_checkpoint="models/weights/stage2_with_pretrained/",
 #     )
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=BitsAndBytesConfig(load_in_8bit=True), local_files_only=True)
+#model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=BitsAndBytesConfig(load_in_8bit=True), local_files_only=True)
 
-print("Model loaded.")
+#print("Model loaded.")
 
 # model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
 
 print("Loading tokenizer...")
 
 # Load tokenizer
-tokenizer = load_llama_tokenizer(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+# tokenizer = load_llama_tokenizer(model_name)
 
 print("Tokenizer loaded.")
