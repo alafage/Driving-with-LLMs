96032a077e8cb741c7e55cec9708d2b42cc9f4ce
alafage <adrienlafage@outlook.com>
2024-09-16 13:26:42 +0200

    :construction:

--- eval_sample.py
+++ eval_sample.py
@@ -1,30 +1,40 @@
 import pickle
+import os
 from utils.model_utils import load_llama_tokenizer, load_model
-from utils.training_utils import generate_and_tokenize_prompt, decode_generation_seqeunces
+from utils.training_utils import get_train_val_data, decode_generation_seqeunces
+from train import TrainerWithGeneration
+
 import torch
 
-
+import transformers
 from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 
+transformers.set_seed(42)
 
-with open("av2_sample0.pkl", "rb") as f:
-    input_data = pickle.load(f)
+base_model = "baffo32/decapoda-research-llama-7B-hf"
+data_path = "data/vqa_train_10k.pkl"
+val_data_path = "av2_sample0.pkl"
 
-input_data["frame"] = 0
-input_data["input"] = ""
-input_data["output"] = ""
-input_data["instruction"] = 'You are a certified professional driving instructor and please tell me step by step how to drive a car based on the input scenario.'
+# Load tokenizer
+tokenizer = load_llama_tokenizer(base_model)
 
-print(input_data.keys())
+train_data, val_data = get_train_val_data(
+    data_path,
+    tokenizer,
+    val_data_path=val_data_path,
+    # val_set_size=val_set_size,
+    augment_times=0,
+    load_pre_prompt_dataset=False,
+    vqa=False,
+    eval_only= True,
+    eval_items=["caption", "action"],
+)
 
-tokenizer = load_llama_tokenizer("baffo32/decapoda-research-llama-7B-hf")
-
-tokenized_input = generate_and_tokenize_prompt(tokenizer, input_data, user_input_ids=True)
-print(tokenized_input.keys())
-
+print(f"Train data: {train_data}")
+print(f"Val data: {val_data}")
 
 model = load_model(
-    base_model="baffo32/decapoda-research-llama-7B-hf",
+    base_model=base_model,
     lora_r=16,
     lora_alpha=16,
     lora_dropout=0.05,
@@ -32,27 +42,52 @@ model = load_model(
     resume_from_checkpoint="models/weights/stage2_with_pretrained/",
 )
 
-trainer = Seq2SeqTrainer(
-        model=model,
-        args=Seq2SeqTrainingArguments(
-            output_dir="./eval_output",
-            generation_config=model.generation_config,
-            fp16=True,
-        )
+world_size = int(os.environ.get("WORLD_SIZE", 1))
+ddp = world_size != 1
+
+trainer = TrainerWithGeneration(
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=val_data,
+    args=Seq2SeqTrainingArguments(
+        per_device_train_batch_size=32,
+        gradient_accumulation_steps=128//32,
+        warmup_ratio=0.04,
+        lr_scheduler_type="cosine",
+        num_train_epochs=5,
+        learning_rate=3e-4,
+        fp16=True,
+        logging_steps=2,
+        optim="adamw_torch",
+        eval_strategy="steps",
+        save_strategy="steps",
+        eval_steps=10,
+        save_steps=200,
+        output_dir="./eval_output",
+        save_total_limit=3,
+        load_best_model_at_end=True,
+        ddp_find_unused_parameters=False if ddp else None,
+        group_by_length=False,
+        report_to=None,
+        run_name=None,
+        label_names=[
+            "route_descriptors",
+            "vehicle_descriptors",
+            "pedestrian_descriptors",
+            "ego_vehicle_descriptor",
+            "user_input_ids",
+            "user_attention_mask",
+        ],
+        prediction_loss_only=False,
+        predict_with_generate=True,
+        generation_max_length=384,
+        generation_config=model.generation_config,
+    ),
+    data_collator=transformers.DataCollatorForSeq2Seq(
+        tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+    ),
+    vqa=True,
 )
 
-for key in ['input_ids', 'attention_mask', 'labels', 'user_input_ids', 'user_attention_mask', 'route_descriptors', 'vehicle_descriptors', 'pedestrian_descriptors', 'ego_vehicle_descriptor']:
-    tokenized_input[key] = torch.tensor(tokenized_input[key], device=model.device)
-
-tokenized_input["input_ids"] = tokenized_input["input_ids"].unsqueeze(0)
-tokenized_input["attention_mask"] = tokenized_input["attention_mask"].unsqueeze(0)
-tokenized_input["labels"] = tokenized_input["labels"].unsqueeze(0)
-
-
-outputs = trainer._wrap_model(model)(**tokenized_input)
-
-print(f"Logits: {outputs['loss']['logits']}")
-
-prediction = decode_generation_seqeunces(tokenizer, outputs["loss"]["logits"][0].detach().cpu())
-
-print(f"Prediction: {prediction}")
+outputs = trainer.evaluate()
+print(outputs)
